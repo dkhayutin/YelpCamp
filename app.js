daa5148951a7a3eda7bc5588e9d47b573afacaae
Dan Khayutin <dan.khayutin@gmail.com>
2018-06-26 12:21:35 -0400

    added campground page, changed landing page and added campsite info

--- app.js
+++ app.js
@@ -9,12 +9,14 @@ app.get('/', function(req, res) {
 
 });
 
-app,get('/campgrounds', function(req, res) {
+app.get('/campgrounds', function(req, res) {
   var campgrounds = [
     {name: 'Salmon Creek', image: "https://pixabay.com/get/e83db50a21f4073ed1584d05fb1d4e97e07ee3d21cac104496f0c179afeeb7bb_340.jpg"},
-    {name: 'Granite Hill',image: "https://pixabay.com/get/e83db50a21f4073ed1584d05fb1d4e97e07ee3d21cac104496f0c179afeeb7bb_340.jpg"},
-    {name: 'Mountain Goat', image: "https://pixabay.com/get/e83db50a21f4073ed1584d05fb1d4e97e07ee3d21cac104496f0c179afeeb7bb_340.jpg"}
+    {name: 'Granite Hill',image: "https://pixabay.com/get/e83db7082af3043ed1584d05fb1d4e97e07ee3d21cac104496f0c179afeeb7bb_340.jpg"},
+    {name: 'Mountain Goat', image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104496f0c179afeeb7bb_340.jpg"}
   ]
+
+  res.render('campgrounds', {campgrounds: campgrounds})
 })
 
 
