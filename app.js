916bfccddf2676460da1fec6cde7e93b67bf5ffb
Dan Khayutin <dan.khayutin@gmail.com>
2018-07-18 20:54:46 -0400

    added id route and description for each campsite, added description placeholder to form and show file

--- app.js
+++ app.js
@@ -14,14 +14,16 @@ app.use (bodyParser.urlencoded({extended: true}));
 
 var campgroundSchema = new mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String
 });
 
 var Campground = mongoose.model('Campground', campgroundSchema)
 // Campground.create(
 //   {
 //     name: 'Granite Hill',
-//     image: "https://farm6.staticflickr.com/5592/14940388216_2d72964061.jpg"
+//     image: "https://farm6.staticflickr.com/5592/14940388216_2d72964061.jpg",
+//     description: 'This is a huge granite hill, no bathrooms, no water. Sweet beach though!'
 // }, function(err, campground) {
 //   if(err) {
 //     console.log('Error')
@@ -41,7 +43,7 @@ app.get('/campgrounds', function(req, res) {
     if(err) {
       console.log('Error')
     } else {
-      res.render('campgrounds', {campgrounds: allCampgrounds})
+      res.render('index', {campgrounds: allCampgrounds})
 
     }
   });
@@ -51,10 +53,23 @@ app.get('/campgrounds/new', function (req, res) {
   res.render('new.ejs')
 });
 
+app.get('/campgrounds/:id', function (req, res) {
+  //find campground with provided id
+  Campground.findById(req.params.id, function(err, foundCampground) {
+    if(err){
+      console.log('Error')
+    } else {
+      //render show template with that campground
+      res.render('show', {campground: foundCampground})
+    }
+  });
+});
+
 app.post('/campgrounds', function(req, res) {
   var name = req.body.name
   var image = req.body.image;
-  var newCampground = {name: name, image: image}
+  var desc = req.body.description
+  var newCampground = {name: name, image: image, description: desc  }
   // create a new campground and save to database
   Campground.create(newCampground, function (err, newlyCreated) {
     if(err){
